bd8ff7e5c5fc93b3b926624b278213f8ed1e1e1b
shardouineau <simon.hardouineau@hotmail.fr>
2016-01-21 11:15:57 +0100

    add Forme mode

--- example2/index.php
+++ example2/index.php
@@ -49,7 +49,7 @@
     <canvas id="c" width="500" height="500" style="border:1px solid #aaa"></canvas>
 
     <div style="display: inline-block; margin-left: 10px">
-        <button id="drawing-mode" class="btn btn-info">Cancel drawing mode</button><br>
+        <button id="drawing-mode" class="btn btn-info">Mode forme</button><br>
         <button id="clear-canvas" class="btn btn-info">Clear</button><br>
 
         <div id="drawing-mode-options">
@@ -67,6 +67,12 @@
             <label for="drawing-color">Line color:</label>
             <input type="color" value="#005E7A" id="drawing-color"><br>
         </div>
+
+        <div id="forme-mode-options" style="display:none">
+            <input type="button" value="Carré" id="square"/>
+            <input type="button" value="Cercle" id="circle"/>
+            <input type="button" value="Triangle" id="triangle"/>
+        </div>
     </div>
 
     <script>
@@ -81,7 +87,8 @@
 
             var drawingModeEl = $('drawing-mode'),
                 drawingOptionsEl = $('drawing-mode-options'),
-                drawingColorEl = $('drawing-color'),
+                formeOptionsEl = $('forme-mode-options'),
+                drawingColorEl = $('drawing-color')
                 drawingLineWidthEl = $('drawing-line-width'),
                 clearEl = $('clear-canvas');
 
@@ -90,12 +97,14 @@
             drawingModeEl.onclick = function() {
                 canvas.isDrawingMode = !canvas.isDrawingMode;
                 if (canvas.isDrawingMode) {
-                    drawingModeEl.innerHTML = 'Cancel drawing mode';
+                    drawingModeEl.innerHTML = 'Mode forme';
                     drawingOptionsEl.style.display = '';
+                    formeOptionsEl.style.display = 'none';
                 }
                 else {
-                    drawingModeEl.innerHTML = 'Enter drawing mode';
+                    drawingModeEl.innerHTML = 'Mode dessin';
                     drawingOptionsEl.style.display = 'none';
+                    formeOptionsEl.style.display = '';
                 }
             };
 
@@ -139,6 +148,27 @@
                 canvas.freeDrawingBrush.color = drawingColorEl.value;
                 canvas.freeDrawingBrush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
             }
+
+            jQuery('#square').click(function(){
+                var square = new fabric.Rect({
+                    width: 20, height: 20, fill: drawingColorEl.value, left: 100, top: 100
+                });
+                canvas.add(square);
+            });
+
+            jQuery('#circle').click(function(){
+                var circle = new fabric.Circle({
+                    radius : 20, fill: drawingColorEl.value, left: 100, top: 100
+                });
+                canvas.add(circle);
+            });
+
+            jQuery('#triangle').click(function(){
+                var triangle = new fabric.Triangle({
+                    width: 20, height: 20, fill: drawingColorEl.value, left: 100, top: 100
+                });
+                canvas.add(triangle);
+            });
         })();
     </script>
 
